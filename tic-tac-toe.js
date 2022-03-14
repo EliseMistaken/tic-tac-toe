c7deafede724d42c47c7c14c85a2f815978bd441
EliseMistaken <lmayo5678@gmail.com>
2022-03-14 14:21:06 -0700

    JS for interacting with gameboard model

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -10,20 +10,49 @@
         Unless we're doing computer AI, that would probably need it's own module. 
 */
 function gameBoard() {
-    /*
-        Needs game state, check for win, change a square, check square value, valid move check, check for tie
-    */
-    let board = Array(9).fill('');
+    
+    let board = Array(9).fill(''); //board is flattened, top left index is 0, incrementing across the row. bottom right corner index is 8
+    let numMoves = 0;
+
+    // False = no winner, true = winner
+    function checkWin() {
+        for( let i = 0; i < 9; i += 3){
+            if(board[i] == board[i+1] == board[i+2] && board[i]) return true;
+        }
+        for (let i = 0 ; i<3; i++){
+            if(board[i] == board[i+3] == board[i+6] && board[i]) return true;
+        }
+        return (board[0] == board[4] == board[8] && board[0])|| (board[2] == board[4] == board[6] && board[2]);
+    }
+    
+    function checkTie() {
+        return numMoves == 9 && !checkWin()
+    }
+
+    function move(position, marker) {
+        if(board[position]) return false
+            
+        board[position] = marker;
+        numMoves++;
+        return true;
+    }
 
     return {
-
+        move,
+        checkTie,
+        checkWin
     }
 };
 
+function game() {
+    //Should set up event listeners, handle game vs AI and pvp. Controller for the model(gameboard). 
+    
+    let board = gameBoard();
+
+}
+
 function player() {
-    /*
-        Needs
-    */
+    
     
     return {
 
